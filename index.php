c4ba3fca36fd716147bfed2312f80a2dfb309be6
Shafayet92 <mdshafayethossain92@gmail.com>
2022-07-28 02:23:28 +0800

    28 jul 2022

--- index.php
+++ index.php
@@ -40,9 +40,6 @@ error_reporting(0);
 
 <body>
 
-  <!-- Start Switcher -->
-  <?php include('includes/colorswitcher.php'); ?>
-  <!-- /Switcher -->
 
   <!--Header-->
   <?php include('includes/header.php'); ?>
@@ -56,8 +53,6 @@ error_reporting(0);
           <div class="col-md-5 col-md-push-7">
             <div class="banner_content">
               <h1>Find the right car for you.</h1>
-              <p>We have more than a thousand cars for you to choose. </p>
-              <a href="#" class="btn">Read More <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a>
             </div>
           </div>
         </div>
@@ -74,103 +69,18 @@ error_reporting(0);
         <h2>Find the Best <span>CarForYou</span></h2>
         <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
       </div>
-      <div class="row">
 
-        <!-- Nav tabs -->
-        <div class="recent-tab">
-          <ul class="nav nav-tabs" role="tablist">
-            <li role="presentation" class="active"><a href="#resentnewcar" role="tab" data-toggle="tab">New Car</a></li>
-          </ul>
-        </div>
-        <!-- Recently Listed New Cars -->
-        <div class="tab-content">
-          <div role="tabpanel" class="tab-pane active" id="resentnewcar">
-
-            <?php $sql = "SELECT tblvehicles.VehiclesTitle,tblbrands.BrandName,tblvehicles.PricePerDay,tblvehicles.FuelType,tblvehicles.ModelYear,tblvehicles.id,tblvehicles.SeatingCapacity,tblvehicles.VehiclesOverview,tblvehicles.Vimage1 from tblvehicles join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand";
-            $query = $dbh->prepare($sql);
-            $query->execute();
-            $results = $query->fetchAll(PDO::FETCH_OBJ);
-            $cnt = 1;
-            if ($query->rowCount() > 0) {
-              foreach ($results as $result) {
-            ?>
-
-                <div class="col-list-3">
-                  <div class="recent-car-list">
-                    <div class="car-info-box"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>"><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1); ?>" class="img-responsive" alt="image"></a>
-                      <ul>
-                        <li><i class="fa fa-car" aria-hidden="true"></i><?php echo htmlentities($result->FuelType); ?></li>
-                        <li><i class="fa fa-calendar" aria-hidden="true"></i><?php echo htmlentities($result->ModelYear); ?> Model</li>
-                        <li><i class="fa fa-user" aria-hidden="true"></i><?php echo htmlentities($result->SeatingCapacity); ?> seats</li>
-                      </ul>
-                    </div>
-                    <div class="car-title-m">
-                      <h6><a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>"><?php echo htmlentities($result->BrandName); ?> , <?php echo htmlentities($result->VehiclesTitle); ?></a></h6>
-                      <span class="price">$<?php echo htmlentities($result->PricePerDay); ?> /Day</span>
-                    </div>
-                    <div class="inventory_info_m">
-                      <p><?php echo substr($result->VehiclesOverview, 0, 70); ?></p>
-                    </div>
-                  </div>
-                </div>
-            <?php }
-            } ?>
-
-          </div>
-        </div>
-      </div>
   </section>
   <!-- /Resent Cat -->
 
-  <!-- Fun Facts-->
-  <section class="fun-facts-section">
-    <div class="container div_zindex">
-      <div class="row">
-        <div class="col-lg-3 col-xs-6 col-sm-3">
-          <div class="fun-facts-m">
-            <div class="cell">
-              <h2><i class="fa fa-calendar" aria-hidden="true"></i>40+</h2>
-              <p>Years In Business</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-xs-6 col-sm-3">
-          <div class="fun-facts-m">
-            <div class="cell">
-              <h2><i class="fa fa-car" aria-hidden="true"></i>1200+</h2>
-              <p>New Cars For Sale</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-xs-6 col-sm-3">
-          <div class="fun-facts-m">
-            <div class="cell">
-              <h2><i class="fa fa-car" aria-hidden="true"></i>1000+</h2>
-              <p>Used Cars For Sale</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-xs-6 col-sm-3">
-          <div class="fun-facts-m">
-            <div class="cell">
-              <h2><i class="fa fa-user-circle-o" aria-hidden="true"></i>600+</h2>
-              <p>Satisfied Customers</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Dark Overlay-->
-    <div class="dark-overlay"></div>
-  </section>
-  <!-- /Fun Facts-->
+
 
 
   <!--Testimonial -->
   <section class="section-padding testimonial-section parallex-bg">
     <div class="container div_zindex">
       <div class="section-header white-text text-center">
-        <h2>Our Satisfied <span>Customers</span></h2>
+        <h2>Everything in life is somewhere else, and you get there in a car.</h2>
       </div>
       <div class="row">
         <div id="testimonial-slider">
